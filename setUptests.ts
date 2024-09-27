0281a85642d7838519aa2f9b71d92788b7b69210
wooBottle <dkrnfls@gmail.com>
2024-09-28 00:05:18 +0900

    remove unused import

--- setUptests.ts
+++ setUptests.ts
@@ -1,6 +1,5 @@
-import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
-import { expect, afterEach } from 'vitest';
+import { afterEach } from 'vitest';
 
 // Extend Vitest's expect method with methods from react-testing-library
 // expect.extend(matchers);
